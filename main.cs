b82d05d9ebac61334046273e3f19cd70269a3912
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-01 23:17:41 -0500

    c#

--- main.cs
+++ main.cs
@@ -14,7 +14,7 @@ class Program
         double deliver = 15;
         string messageAnswer = null;
         string methodAnswer = null;
-          
+   
         Console.WriteLine("Welcome to the Yummy Yummy Bakery Calculator");
         Console.WriteLine("\nAnswer the following questions to find out the cost of your small chocolate cake.");
         Console.Write("\nDo you want a special message ('Yes' or 'No')? ");
@@ -22,14 +22,18 @@ class Program
         Console.Write("\nPick up or Delivery ('Pick Up' or 'Delivery')? ");
         methodAnswer = Console.ReadLine();
 
-        if (messageAnswer == "Yes" && methodAnswer == "Delivery"){
+        if (messageAnswer == "Yes" && methodAnswer == "Delivery")
+        {
             Console.Write("\nThe cost of your small chocolate cake is $" + ((price + message + deliver) * tax).ToString("0.00") + ".");
-        } else if (messageAnswer == "No" && methodAnswer == "Delivery"){
-          Console.Write("\nThe cost of your small chocolate cake is $" + ((price + deliver) * tax).ToString("0.00") + ".");
-        } else if (messageAnswer == "Yes" && methodAnswer == "Pick Up"){
-          Console.Write("\nThe cost of your small chocolate cake is $" + ((price + message) * tax).ToString("0.00") + ".");
-        } else if (messageAnswer == "No" && methodAnswer == "Pick Up"){
-          Console.Write("\nThe cost of your small chocolate cake is $" + (price * tax).ToString("0.00") + ".");
+        }else if (messageAnswer == "No" && methodAnswer == "Delivery")
+        {
+            Console.Write("\nThe cost of your small chocolate cake is $" + ((price + deliver) * tax).ToString("0.00") + ".");
+        }else if (messageAnswer == "Yes" && methodAnswer == "Pick Up"){
+          
+            Console.Write("\nThe cost of your small chocolate cake is $" + ((price + message) * tax).ToString("0.00") + ".");
+        }else if (messageAnswer == "No" && methodAnswer == "Pick Up")
+        {
+            Console.Write("\nThe cost of your small chocolate cake is $" + (price * tax).ToString("0.00") + ".");
         }
        Console.Write("\nDone");
     }
